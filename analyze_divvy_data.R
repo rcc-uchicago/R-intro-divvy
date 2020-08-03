b41e38aa510a7b5eb2f1bc15d3a53954619d0875
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-08-03 16:04:28 -0500

    Added initial steps to inspect station data in R script.

--- analyze_divvy_data.R
+++ analyze_divvy_data.R
@@ -1,7 +1,50 @@
 # TO DO: Summarize contents of script here.
 
 # Load station data.
-# TO DO.
+stations <- read.csv("Divvy_Stations_2017_Q3Q4.csv",
+                     stringsAsFactors = FALSE)
+
+# Inspect the table.
+nrow(stations)
+ncol(stations)
+head(stations)
+tail(stations)
+summary(stations)
+str(stations)
+
+# Inspect the data in more detail.
+sapply(stations,class)
+object.size(stations)
+
+# Select only the "dpcapacity" column, and assign it to a new variable:
+x <- stations$dpcapacity
+
+# Run these commands to take a closer look at the "dpcapacity" column:
+class(x)
+length(x)
+summary(x)
+min(x)
+max(x)
+mean(x)
+median(x)
+quantile(x,0.5)
+table(x)
+
+# It is interesting that a couple of the Divvy bike stations have no
+# docks. What are these stations?
+subset(stations,dpcapacity == 0)
+stations[stations$dpcapacity == 0,]
+
+# Previously, we inspected numeric data. Next’s, let’s look at an
+# example of non-numeric data.
+x <- stations$city
+class(x)
+summary(x)
+
+# Convert "city" to a "factor".
+x <- factor(stations$city)
+class(x)
+summary(x)
 
 # Load trip data.
 # TO DO.
