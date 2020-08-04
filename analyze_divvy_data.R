831f14b70e6f6b00d2e9522201a8259a143c4792
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-08-03 21:04:15 -0500

    Added plots showing number of trips per day, and number of trips by station.

--- analyze_divvy_data.R
+++ analyze_divvy_data.R
@@ -92,3 +92,23 @@ source("read_trip_data.R")
 
 # Inspect trip data.
 # TO DO.
+
+# Create a bar chart using the "barplot" showing number of trips per
+# day.
+counts <- table(trips$dayofyear)
+plot(274:365,counts,pch = 20)
+
+# Create a bar chart using ggplot.
+pdat <- data.frame(day = 274:365,counts = as.vector(counts))
+p3 <- ggplot(pdat,aes(x = day,y = counts)) +
+  geom_point() +
+  theme_cowplot()
+
+# Plot a map of the stations in which the area is proportional to the
+# number of trips taken from that station.
+counts         <- table(trips$from_station_name)
+stations$count <- as.vector(counts[stations$name])
+p4 <- ggplot(stations,aes(x = longitude,y = latitude,size = sqrt(count))) +
+  geom_point(shape = 21,color = "white",fill = "dodgerblue") +
+  theme_cowplot()
+print(p4)
--- read_trip_data.R
+++ read_trip_data.R
@@ -1,24 +1,3 @@
-# Read Divvy station data from a CSV file. I've only tested this code
-# on the 2016 and 2017 Divvy data; data from other years may have
-# slightly different formats.
-read.station.data  <- function (filename) {
-
-  # Load the station data.
-  cat("Reading station data from ",filename,".\n",sep="")
-  dat <- read.csv(filename,header = TRUE,stringsAsFactors = FALSE)
-  class(dat) <- "data.frame"
-
-  # Trim the whitespace from the cities column, then convert the
-  # "city" column to a factor.
-  dat <- transform(dat,city = factor(trimws(city)))
-
-  # Strip any columns that are all empty, and return the data frame.
-  cols <- sapply(dat,function (x) !all(is.na(x)))
-  dat  <- dat[cols]
-
-  return(dat)
-}
-
 # Read Divvy trip and station data from CSV files, and combine these
 # data into two data frames (one data frame for the trip data, and
 # another data frame for the station data).
@@ -39,55 +18,18 @@ read.station.data  <- function (filename) {
 #
 # Also note that only the trip start times are retained; the trip end
 # times are removed.
-read.trip.data <- function (trip.files, stations) {
+trips <- read_csv("Divvy_Trips_2019_Q4.csv.gz")
+class(trips) <- "data.frame"
 
-  # Figure out whether to use read.csv or read_csv.
-  if (requireNamespace("readr",quietly = TRUE))
-    use.readr <- TRUE
-  else
-    use.readr <- FALSE
-  
-  # Combine the trip data into a single table. Some of the column
-  # names do not match up exactly, so we need to fix them.
-  n     <- length(trip.files)
-  trips <- NULL
-  for (i in 1:n) {
-    cat("Reading trip data from ",trip.files[i],".\n",sep="")
-    if (use.readr) {
-      suppressMessages(x <- readr::read_csv(trip.files[i],progress = FALSE))
-      class(x) <- "data.frame"
-    } else
-      x <- read.csv(trip.files[i],header = TRUE,stringsAsFactors = FALSE)
-    names(x) <- c("trip_id","start_time","end_time","bikeid","tripduration",
-                  "from_station_id","from_station_name","to_station_id",
-                  "to_station_name","usertype","gender","birthyear")
-    trips <- rbind(trips,x)
-  }
+# Convert the gender, usertype, from_station_name and to_station_name
+# columns to factors.
+trips$gender            <- factor(trips$gender)
+trips$usertype          <- factor(trips$usertype)
+trips$from_station_name <- factor(trips$from_station_name)
+trips$to_station_name   <- factor(trips$to_station_name)
 
-  # Remove the stop times, and the "from" and "to" station ids from the
-  # trip data.
-  trips <- trips[-c(3,6,8)]
-  
-  # Set some of the table columns to factors.
-  cat("Preparing combined trip data.\n")
-  trips$gender[trips$gender == ""] <- NA
-  trips <-
-    transform(trips,
-              usertype          = factor(usertype),
-              gender            = factor(gender),
-              bikeid            = factor(bikeid),
-              from_station_name = factor(from_station_name,stations$name),
-              to_station_name   = factor(to_station_name,stations$name))
+# Create a "day of year" column.
+trips$dayofyear <- as.numeric(format(trips$start_time,"%j"))
 
-  # Convert the start times from character strings to dates (here I'm
-  # following the suggestions made by Larry Layne and Austin Wehrwein).
-  cat("Extracting years from start times.\n")
-  trips <- transform(trips,
-             start_time = strptime(start_time,format = "%m/%d/%Y %H:%M"))
-  trips <- transform(trips,
-             start.year = factor(as.numeric(format(start_time,"%Y"))))
-  
-  # Return a list object containing the station data and the trip
-  # data.
-  return(trips)
-}
+# Convert "day of year" column to factor.
+trips$dayofyear <- factor(trips$dayofyear)
